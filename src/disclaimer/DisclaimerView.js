e87ad4d4a07a16548f9d1996dc92aaf9c428bc01
Vladislav <sorokinvld@gmail.com>
2022-05-19 20:56:47 +0300

    cmt

--- src/disclaimer/DisclaimerView.js
+++ src/disclaimer/DisclaimerView.js
@@ -14,16 +14,16 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import {TemplateView} from "../utils/TemplateView.js";
+import { TemplateView } from "../utils/TemplateView.js";
 
 export class DisclaimerView extends TemplateView {
     render(t) {
         return t.div({ className: "DisclaimerView card" }, [
             t.h1("Disclaimer"),
             t.p(
-                'Matrix.to is a service provided by the Matrix.org Foundation ' +
+                'Dialog is a service provided by the Matrix.org Foundation ' +
                 'which allows you to easily create invites to Matrix rooms and accounts, ' +
-                'regardless of your Matrix homeserver. The service is provided "as is" without ' +
+                'regardless of your Dialog homeserver. The service is provided "as is" without ' +
                 'warranty of any kind, either express, implied, statutory or otherwise. ' +
                 'The Matrix.org Foundation shall not be responsible or liable for the room ' +
                 'and account contents shared via this service.'
